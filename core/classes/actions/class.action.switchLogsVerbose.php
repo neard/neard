2b11598fbc04d30203b8059977332f197117726b
CrazyMax <crazy-max@users.noreply.github.com>
2016-12-17 21:22:39 +0100

    Cannot switch to trace verbosity (Issue #198)

--- core/classes/actions/class.action.switchLogsVerbose.php
+++ core/classes/actions/class.action.switchLogsVerbose.php
@@ -6,7 +6,7 @@ class ActionSwitchLogsVerbose
     {
         global $neardConfig;
         
-        if (isset($args[0]) && is_numeric($args[0]) && $args[0] >= 0 && $args[0] <= 2) {
+        if (isset($args[0]) && is_numeric($args[0]) && $args[0] >= 0 && $args[0] <= 3) {
             $neardConfig->replace(Config::CFG_LOGS_VERBOSE, $args[0]);
         }
     }
